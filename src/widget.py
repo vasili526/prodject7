3683afd44cdc800a436211b5d920b04e7eddd58d
Vasiliy Tatarchenko <vasiliyrevih.456@gmail.com>
2025-11-07 18:00:26 +0300

    git init

--- src/widget.py
+++ src/widget.py
@@ -10,3 +10,15 @@ def mask_account_card(card_info):
         return " Счет " + get_mask_account(namber)
     return ' '.join(papse[:-1]) +" "+ get_mask_card_number(namber)
 
+
+
+def get_date(date_str: str) -> str:
+    """
+    Принимает строку в формате '2024-03-11T02:26:18.671407'
+    и возвращает строку в формате 'ДД.ММ.ГГГГ' (например '11.03.2024').
+    """
+    # Парсим строку в объект datetime
+    dt = datetime.fromisoformat(date_str)
+
+    # Возвращаем в нужном формате
+    return dt.strftime("%d.%m.%Y")
